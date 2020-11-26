4d6a9dca9a0f15baab1bc2ca5f34c526d39be5ad
Tomonori_Yamanae <t-yamne@spit.co.jp>
2020-11-26 23:04:43 +0900

    learn propstypes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-
+import PropTypes from 'prop-types';
 
 const App = () => {
     const profiles = [
       {name: 'Tomonori',age: 28},
       {name: 'Miu',age: 5},
-      {name: 'Taro'},
+      {name: 'Taro', age: 6},
     ]
     return (
       <div>
@@ -26,8 +25,9 @@ const User = (props) => {
     );
 }
 
-User.defaultProps = {
-  age:1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
