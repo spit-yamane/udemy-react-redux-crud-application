452f4b11dbe752732d06f4c4d75915e1fa322088
Tomonori_Yamanae <t-yamne@spit.co.jp>
2020-11-26 22:10:56 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
-        <h1>hello world</h1>
-    </div>
-  );
+import {Component} from 'react';
+
+class App extends Component{
+  render(){
+
+    return(
+      <>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=>{console.log("click!")}} />
+      </>
+    )
+  }
 }
+// function App() {
+//     return (
+//       <h1>hello world</h1>
+//     );
+// }
 
 export default App;
