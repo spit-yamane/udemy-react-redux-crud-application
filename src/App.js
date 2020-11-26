36feaf79655f210e2cf490584af4c785ceead721
Tomonori_Yamanae <t-yamne@spit.co.jp>
2020-11-26 23:31:00 +0900

    lean state and setstate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component }  from 'react';
 
-const App = () => {
-    const profiles = [
-      {name: 'Tomonori',age: 28},
-      {name: 'Miu',age: 5},
-      {name: 'Taro', age: 6},
-    ]
-    return (
-      <div>
-        {
-          profiles.map((profile ,index)=>{
-            return <User name={profile.name} age={profile.age} key={index}/>
-          })
-        }
-      </div>
-    );
-}
-const User = (props) => {
+const App = () => (<Counter/>)
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render(){
     return (
       <>
-         <div>Hi! I am {props.name}! , and {props.age} years old.</div>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
       </>
-    );
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+    )
+  }
 }
 
 export default App;
