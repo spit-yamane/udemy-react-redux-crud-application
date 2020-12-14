91973dec8e0577b566da6e84f75c26d5720b841c
Tomonori_Yamanae <t-yamne@spit.co.jp>
2020-12-15 00:34:14 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUEYSTRING = '?token=token123'
@@ -9,4 +10,9 @@ const QUEYSTRING = '?token=token123'
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUEYSTRING}`)
     dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/events${QUEYSTRING}`, values)
+    dispatch({ type: CREATE_EVENT, response })
 }
--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,14 +1,15 @@
 import React, { Component }  from 'react';
 import { connect } from 'react-redux';
-import { readEvents} from '../actions';
+import { readEvents } from '../actions';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
-class EventsIndex  extends Component {
+class EventsIndex extends Component {
 
   componentDidMount(){
     this.props.readEvents()
   }
-  readEvents(){
+  renderEvents(){
     return _.map(this.props.events, event => (
       <tr key={event.id}>
         <td>{event.id}</td>
@@ -19,23 +20,27 @@ class EventsIndex  extends Component {
   }
   render(){
     return (
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.readEvents()}
-          </tbody>
-        </table>
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderEvents()}
+            </tbody>
+          </table>
+
+          <Link to="/events/new">New Event</Link>
+        </>
     )
   }
 }
 
-const mapStateToProps = state => ({ events: state.events})
+const mapStateToProps = state => ({ events: state.events })
 
 const mapDispacthToProps = ({ readEvents })
 
